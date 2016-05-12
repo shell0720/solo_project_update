9159d9cf54b0aadd2eb4995183b8bcd187bb05cf
Michelle Wang <shell0720@gmail.com>
2016-05-12 09:42:28 -0500

    update cookie

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(session({
     key: "user",
     resave: true,
     s: false,
-    cookie: {maxAge: 60000, secure: false}
+    cookie: {maxAge: 6000000000000000, secure: false}
 }));
 
 app.use(cookieParser());
