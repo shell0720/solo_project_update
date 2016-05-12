aafde63a25d298c71c079c1aa6a9f478a6777997
Michelle Wang <shell0720@gmail.com>
2016-05-12 13:47:36 -0500

    update heroku

--- server/routes/db.js
+++ server/routes/db.js
@@ -3,9 +3,9 @@ var mongoose = require("mongoose");
 
 var mongoURI =
  process.env.MONGOLAB_URI ||
- //process.env.MONGOHQ_URL ||
-//'mongodb://localhost/kappa_solo'||
- 'mongodb://Michelle:ds013192.mlab.com:13192/heroku_q4vl9pg4';
+ process.env.MONGOHQ_URL ||
+ 'mongodb://localhost/kappa_solo'||
+ 'mongodb://Michelle:@ds013192.mlab.com:13192/heroku_q4vl9pg4';
 
 var MongoDB = mongoose.connect(mongoURI).connection;
 
