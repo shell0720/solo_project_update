1a4a46ef277dfa463318246953f3d931bcfa0d7a
Michelle Wang <shell0720@gmail.com>
2016-05-12 13:59:22 -0500

    push heroku

--- server/routes/db.js
+++ server/routes/db.js
@@ -2,12 +2,17 @@ var mongoose = require("mongoose");
 
 
 var mongoURI =
- process.env.MONGOLAB_URI ||
- process.env.MONGOHQ_URL ||
+ // process.env.MONGOLAB_URI ||
+ // process.env.MONGOHQ_URL ||
  'mongodb://localhost/kappa_solo'||
  'mongodb://Michelle:@ds013192.mlab.com:13192/heroku_q4vl9pg4';
 
-var MongoDB = mongoose.connect(mongoURI).connection;
+
+mongoose.connect(mongoURI);
+
+var MongoDB = mongoose.connection;
+
+//var MongoDB = mongoose.connect(mongoURI).connection;
 
 MongoDB.on("error", function(err){
     console.log("Mongo Connection Error: ", err);
