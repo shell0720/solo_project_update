17a59d10a9ef00031b99a4b52e79a96e4ea37c07
shell0720 <shell0720@gmail.com>
2016-05-13 09:25:02 -0500

    Update db.js

--- server/routes/db.js
+++ server/routes/db.js
@@ -2,18 +2,12 @@ var mongoose = require("mongoose");
 
 
 var mongoURI =
-<<<<<<< HEAD
- process.env.MONGOLAB_URI ||
- process.env.MONGOHQ_URL ||
-'mongodb://localhost/kappa_solo'||
- 'mongodb://Michelle:ds013192.mlab.com:13192/heroku_q4vl9pg4';
-=======
+
   //process.env.MONGOLAB_URI ||
   //process.env.MONGOHQ_URL ||
  //'mongodb://localhost/kappa_solo'||
  'mongodb://Xiaoran:123@ds013192.mlab.com:13192/heroku_q4vl9pg4';
 
->>>>>>> 40cfde870767457aceacaa1163621e75ee86f3f0
 
 var MongoDB = mongoose.connect(mongoURI).connection;
 
